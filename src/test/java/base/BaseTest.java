3ff759c9a317a0e34f1dd984684d7121b0533369
Luca Vinelli <lucavinelli4@gmail.com>
2025-10-02 15:12:40 -0300

    feat: add base test, package structure and allure properties

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,12 @@
+package base;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+    }
+}
